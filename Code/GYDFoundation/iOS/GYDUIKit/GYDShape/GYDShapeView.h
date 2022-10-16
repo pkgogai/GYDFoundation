6a9e3397573a00bc10d9e2c0c4f5559f41d483ed
宫亚东 <gongyadong@lagou.com>
2022-10-16 14:15:26 +0800

    备注

--- Code/GYDFoundation/iOS/GYDUIKit/GYDShape/GYDShapeView.h
+++ Code/GYDFoundation/iOS/GYDUIKit/GYDShape/GYDShapeView.h
@@ -9,10 +9,12 @@
 #import <UIKit/UIKit.h>
 #import "GYDShape.h"
 
+/**
+ self.backgroundColor必须有透明度，否则系统认定这个view的颜色是不透明的，就不处理透明情况了，绘制的透明图形也就直接变成了黑色。具体要怎样才能让系统强制按有透明度来处理呢？opaque等设置没用。
+ */
 @interface GYDShapeView : UIView
 
 - (void)addShape:(GYDShape *)shape;
 - (void)removeShape:(GYDShape *)shape;
 
 @end
-
